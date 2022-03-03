9df22b208098af94d5879ff41cd4f210070e00ac
PabloDeLaCruz1 <pablo.de.la.cruz.pro@gmail.com>
2022-03-02 20:17:19 -0500

    Setting Up and Outlining

---
+++ E-Commerse-iOS/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  E-Commerse-iOS
+//
+//  Created by Pablo De La Cruz on 3/2/22.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
